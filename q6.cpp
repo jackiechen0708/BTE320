746406060981c564e20efa3c0eb3c0c7ae5d5602
jackiechen0708 <12307130250@fudan.edu.cn>
2018-12-11 17:03:04 +0800

    Update q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -3,20 +3,26 @@
 using namespace std;
 int array[10];
 
-void search(int n) {
+int search(int n) {
+    int result = 0;
     for (int i = 0; i < 10; i++) {
         if (array[i] == n) {
-            cout << "Position:" << i << " occurs " << n << endl;
+            //cout << "Position:" << i << " occurs " << n << endl;
+            result += 1;
         }
     }
+    return result;
 }
 
 int main() {
 
     int tmp;
+    cout <<"Please input 10 numbers:"<<endl;
     for (int i = 0; i < 10; i++) {
         cin >> tmp;
         array[i] = tmp;
     }
-    search(2);
+    cout <<"Please input the number to search"<<endl;
+    cin >> tmp;
+    cout <<"number " << tmp <<" occurs "<<search(tmp)<<" times"<<endl;
 }
