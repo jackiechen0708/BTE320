734002fa1ddb9dc15d4ab23eb03a57734350f07d
jackiechen0708 <12307130250@fudan.edu.cn>
2018-12-11 13:54:20 +0800

    Update q8.cpp

--- q8.cpp
+++ q8.cpp
@@ -1,213 +1,221 @@
-#include <iostream>
 #include <sstream>
 #include <string>
-//TODO: format outputs and change the variables
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
+const double TAX_RATIO = 0.065;
+const double TIP_RATIO = 0.2;
+
+struct meal {
+	string name;
+	double price;
+};
+constexpr int NUM_OF_APPETIZERS = 4;
+constexpr int NUM_OF_ENTREES = 5;
+constexpr int NUM_OF_DRINKS = 3;
+constexpr int NUM_OF_DESERTS = 3;
+
+meal appetizers[NUM_OF_APPETIZERS] = {
+	{ "Chicken Satay",7.50 },
+{ "Egyptian Spring Rolls",6.50 },
+{ "Humus",5.50 },
+{ "Samboosa",5.50 }
+};
+
+meal entrees[NUM_OF_ENTREES] = {
+	{ "Shish Tawooq - Chicken Kabaab",13.50 },
+{ "Shish Kabaab- Beef Kabaab",16.50 },
+{ "Fatta Bel Mooza",17.25 },
+{ "Koshary - vegetarian",10.25 },
+{ "Koshary - with meat sauce",12.25 }
+};
+
+meal drinks[NUM_OF_DRINKS] = {
+	{ "Tea with mint",2.25 },
+{ "Turkish Coffee",2.25 },
+{ "Soda",2.00 }
+};
+
+meal deserts[NUM_OF_DESERTS] = {
+	{ "Rice Pudding - with rose water",6.00 },
+{ "Bamboosa - 2 pieces",4.50 },
+{ "Mango Pudding",6.00 }
+};
+
+int main() {
+
+	string input_s = "";
+	int input;
+	stringstream convert;
+	int item_count = 0;
+	string item = "";
+	string cur = "";
+	double Subtotal = 0;
+	double Tax = 0;
+	double Tip = 0;
+	double Total = 0;
 
 
-int main(){
+	while (input_s != "q") {
+		int idx = 1;
+		cout << "\t\tWelcome to the Miami Favorites Meal Builder\t" << endl;
+		cout << "\t\t===========================================\t" << endl;
+		cout << endl;
+		cout << "\t\tNewMean (n)\t\t\tQuit (q)\t" << endl;
+		cout << "\t\t\t\tAppetizers" << endl;
+		cout << endl;
+		for (int i = 0; i < NUM_OF_APPETIZERS; i++) {
+			std::cout << std::left << std::setw(10) << std::left << idx;
+			idx++;
+			std::cout << std::left << std::setw(45) << appetizers[i].name;
+			std::cout << "$" << std::left << std::setw(70) << appetizers[i].price << std::endl;
+		}
+		cout << endl;
+		cout << "\t\t\t\tEntrees" << endl;
+		cout << endl;
+		for (int i = 0; i < NUM_OF_ENTREES; i++) {
+			std::cout << std::left << std::setw(10) << std::left << idx;
+			idx++;
+			std::cout << std::left << std::setw(45) << entrees[i].name;
+			std::cout << "$" << std::left << std::setw(70) << entrees[i].price << std::endl;
+		}
+		cout << endl;
+		cout << "\t\t\t\tDrinks" << endl;
+		cout << endl;
+		for (int i = 0; i < NUM_OF_DRINKS; i++) {
+			std::cout << std::left << std::setw(10) << std::left << idx;
+			idx++;
+			std::cout << std::left << std::setw(45) << drinks[i].name;
+			std::cout << "$" << std::left << std::setw(70) << drinks[i].price << std::endl;
+		}
+		cout << endl;
+		cout << "\t\t\t\tDessert" << endl;
+		cout << endl;
+		for (int i = 0; i < NUM_OF_DESERTS; i++) {
+			std::cout << std::left << std::setw(10) << std::left << idx;
+			idx++;
+			std::cout << std::left << std::setw(45) << deserts[i].name;
+			std::cout << "$" << std::left << std::setw(70) << deserts[i].price << std::endl;
+		}
+		cout << endl;
+		cout << endl;
+		cout << "Please enter item[1-15]: ";
 
-    int input;
-    int count = 0;
-    string item = "";
-    double total = 0;
-    double taxTotal = (total*0.07)/0.01;
-    double tipTotal = (total*0.15)/0.01;
-    double tTotal = (total + taxTotal + tipTotal)/0.01;
+		cin >> input_s;
+		if (input_s == "n") {
+			item.clear();
+			Subtotal = 0;
+			Tax = 0;
+			Tip = 0;
+			Total = 0;
+			item_count = 0;
+		}
+		else if (input_s == "q") {
+			exit(0);
+		}
+		else {
+			convert.str("");
+			convert.clear();
+			convert << input_s;
+			convert >> input;
+			switch (input) {
+			
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				convert.str("");
+				convert.clear();
+				convert << std::left << std::setw(10) << input
+					<< std::left << std::setw(10) << "Appetizers"
+					<< std::left << std::setw(40) << appetizers[input - 1].name
+					<< "$" << appetizers[input - 1].price << endl;
+				item = item + convert.str();
+				Subtotal = Subtotal + appetizers[input - 1].price;
+				Tax = Subtotal * TAX_RATIO;
+				Tip = Subtotal * TIP_RATIO;
+				Total = Subtotal + Tax + Tip;
+				item_count = item_count + 1;
+				break;
+			
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+				input -= 4;
+				convert.str("");
+				convert.clear();
+				convert << std::left << std::setw(10) << input 
+					<< std::left << std::setw(10) << "Entrees"
+					<< std::left << std::setw(40) <<entrees[input - 1].name 
+					<< "$" << entrees[input - 1].price << endl;
+				convert >> cur;
+				item = item + convert.str();
+				Subtotal = Subtotal + entrees[input - 1].price;
+				Tax = Subtotal * TAX_RATIO;
+				Tip = Subtotal * TIP_RATIO;
+				Total = Subtotal + Tax + Tip;
+				item_count = item_count + 1;
+				break;
+			
+			case 10:
+			case 11:
+			case 12:
+				input -= 9;
+				convert.str("");
+				convert.clear();
+				convert << std::left << std::setw(10) << input
+					<< std::left << std::setw(10) << "Drinks"
+					<< std::left << std::setw(40) << drinks[input - 1].name
+					<< "$" << drinks[input - 1].price << endl;
+				convert >> cur;
+				item = item + convert.str();
+				Subtotal = Subtotal + drinks[input - 1].price;
+				Tax = Subtotal * TAX_RATIO;
+				Tip = Subtotal * TIP_RATIO;
+				Total = Subtotal + Tax + Tip;
+				item_count = item_count + 1;
+				break;
+			case 13:
+			case 14:
+			case 15:
+				input -= 12;
+				convert.str("");
+				convert.clear();
+				convert << std::left << std::setw(10) << input
+					<< std::left << std::setw(10) << "Dessert"
+					<< std::left << std::setw(40) << deserts[input - 1].name
+					<< "$" << deserts[input - 1].price << endl;
+				convert >> cur;
+				item = item + convert.str();
+				Subtotal = Subtotal + deserts[input - 1].price;
+				Tax = Subtotal * TAX_RATIO;
+				Tip = Subtotal * TIP_RATIO;
+				Total = Subtotal + Tax + Tip;
+				item_count = item_count + 1;
+				break;
+			default:
+				cout << "Invalid Input" << endl;
+			}
+		}
 
-    cout.precision(2);
+		cout << "=======================================================================\nYour current meal has ";
+		cout << to_string(item_count);
+		cout << " item(s).\n";
+		cout << item;
+		cout << "=======================================================================\n";
+		cout << "\t\t\t\t\t\tSubtotal =  $";
+		cout << Subtotal;
+		cout << "\n\t\t\t\t\t\tTax =  $";
+		cout << Tax;
+		cout << "\n\t\t\t\t\t\tTip =  $";
+		cout << Tip;
+		cout << "\n\t\t\t\t\t\tTotal =  $";
+		cout << Total;
+		cout << endl;
+		cout << endl;
 
-    while (input != 99){
-
-        cout << "\t\tWelcome to the Miami Favorites Meal Builder\t" << endl;
-        cout << "\t\t===========================================\t" << endl;
-        cout << endl;
-        cout << "\t\tNewMean (0)\t\t\tQuit (99)\t" << endl;
-        cout << "\t\t\t\tAppetizers" << endl;
-        cout << endl;
-        cout << "1\tChicken Satay\t\t\t\t\t\t$7.50" << endl;
-        cout << "2\tEgyptian Spring Rolls\t\t\t\t\t$6.50" << endl;
-        cout << "3\tHumus\t\t\t\t\t\t\t$5.50" << endl;
-        cout << "4\tSamboosa\t\t\t\t\t\t$5.50" << endl;
-        cout << endl;
-        cout << "\t\t\t\tEntrees" << endl;
-        cout << endl;
-        cout << "5\tShish Tawooq - Chicken Kabaab\t\t\t\t$13.50" << endl;
-        cout << "6\tShish Kabaab- Beef Kabaab\t\t\t\t$16.50" << endl;
-        cout << "7\tFatta Bel Mooza\t\t\t\t\t\t$17.25" << endl;
-        cout << "8\tKoshary - vegetarian\t\t\t\t\t$10.25" << endl;
-        cout << "9\tKoshary - with meat sauce\t\t\t\t$12.25" << endl;
-        cout << endl;
-        cout << "\t\t\t\tDrinks" << endl;
-        cout << endl;
-        cout << "10\tTea with mint\t\t\t\t\t\t$2.25" << endl;
-        cout << "11\tTurkish Coffee\t\t\t\t\t\t$2.25" << endl;
-        cout << "12\tSoda\t\t\t\t\t\t\t$2.00" << endl;
-        cout << endl;
-        cout << "\t\t\t\tDessert" << endl;
-        cout << endl;
-        cout << "13\tRice Pudding - with rose water\t\t\t\t$6.00" << endl;
-        cout << "14\tBamboosa - 2 pieces\t\t\t\t\t$4.50" << endl;
-        cout << "15\tMango Pudding\t\t\t\t\t\t$6.00" << endl;
-        cout << endl;
-        cout << endl;
-        cout << "Please enter item[1-15]: ";
-
-        cin >> input;
-
-        switch(input) {
-            case 0 :
-                item.clear();
-                total = 0;
-                taxTotal = 0;
-                tipTotal = 0;
-                tTotal = 0;
-                count = 0;
-                break;
-            case 99 :
-                exit(0);
-            case 1 :
-                item = item + "1\tChicken Satay\t\t\t\t\t\t$7.50\n";
-                total = total + 7.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 2 :
-                item = item + "2\tEgyptian Spring Rolls\t\t\t\t\t$6.50\n";
-                total = total + 6.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 3 :
-                item = item + "3\tHumus\t\t\t\t\t\t\t$5.50\n";
-                total = total + 5.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 4 :
-                item = item + "4\tSamboosa\t\t\t\t\t\t$5.50\n";
-                total = total + 5.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 5 :
-                item = item + "5\tShish Tawooq - Chicken Kabaab\t\t\t\t$13.50\n";
-                total = total + 13.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 6 :
-                item = item + "6\tShish Kabaab- Beef Kabaab\t\t\t\t$16.50\n";
-                total = total + 16.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 7 :
-                item = item + "7\tFatta Bel Mooza\t\t\t\t\t\t$17.25\n";
-                total = total + 17.25;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 8 :
-                item = item + "8\tKoshary - vegetarian\t\t\t\t\t$10.25\n";
-                total = total + 10.25;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 9 :
-                item = item + "9\tKoshary - with meat sauce\t\t\t\t$12.25\n";
-                total = total + 12.25;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 10 :
-                item = item + "10\tTea with mint\t\t\t\t\t\t$2.25\n";
-                total = total + 2.25;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-
-                count = count + 1;
-                break;
-            case 11 :
-                item = item + "11\tTurkish Coffee\t\t\t\t\t\t$2.25\n";
-                total = total + 2.25;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 12 :
-                item = item + "12\tSoda\t\t\t\t\t\t\t$2.00\n";
-                total = total + 2.00;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 13 :
-                item = item + "13\tRice Pudding - with rose water\t\t\t\t$6.00\n";
-                total = total + 6.00;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 14 :
-                item = item + "14\tBamboosa - 2 pieces\t\t\t\t\t$4.50\n";
-                total = total + 4.50;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            case 15 :
-                item = item + "15\tMango Pudding\t\t\t\t\t\t$6.00\n";
-                total = total + 6.00;
-                taxTotal = total*0.07;
-                tipTotal = total*0.15;
-                tTotal = total + taxTotal + tipTotal;
-                count = count + 1;
-                break;
-            default :
-                cout << "Invalid Input" << endl;
-        }
-
-        cout.precision(2);
-        cout << "=======================================================================\nYour current meal has ";
-        cout << to_string(count);
-        cout << " item(s).\n";
-        cout << item;
-        cout << "=======================================================================\n";
-        cout << "\t\t\t\t\t\tSubtotal =  $";
-        cout << total;
-        cout << "\n\t\t\t\t\t\tTax =  $";
-        cout << taxTotal;
-        cout << "\n\t\t\t\t\t\tTip =  $";
-        cout << tipTotal;
-        cout << "\n\t\t\t\t\t\tTotal =  $";
-        cout << tTotal;
-        cout << endl;
-        cout << endl;
-
-    }
+	}
 }
